899782bfec52fdc033927e6c3aff844cd83663a4
Mazen-Ghanaym <mznghanayem@gmail.com>
2026-07-17 12:43:16 +0300

    Enable bounds sanitizer for binary-search to catch out-of-bound array accesses (e.g. r=n bug)

--- 7/binary-search/__init__.py
+++ 7/binary-search/__init__.py
@@ -32,7 +32,7 @@ def exists():
 @check50.check(exists)
 def test_compile():
     """binary-search.cpp compiles successfully"""
-    check50.run("g++ binary-search.cpp -o binary-search").exit(0)
+    check50.run("g++ binary-search.cpp -fsanitize=bounds -fno-sanitize-recover=bounds -o binary-search").exit(0)
 
 @check50.check(test_compile)
 def test_example1():
